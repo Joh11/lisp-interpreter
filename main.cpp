de92e392abebd4c1a2f2646249bf6045695c3c89
Joh11 <johan74470@gmail.com>
2017-09-04 14:25:51 +0200

    enhanced interface

--- main.cpp
+++ main.cpp
@@ -10,35 +10,16 @@ int main(int argc, char **argv)
     parser p;
     interpreter i;
 
-    std::string str;
-    std::getline(std::cin, str);
+    while(true)
+    {
+	std::string str;
+	std::getline(std::cin, str);
 
-    std::cout << "Bonjour" << std::endl;
+	if(str == "QUIT")
+	    break;
 
-    auto t = p.parse(str);
-    std::cout << "Bonjour" << std::endl;
-    
-    std::cout << i.interpret(t) << std::endl;
+	std::cout << "==> "  << i.interpret(p.parse(str)) << std::endl;
+    }
 
-    /*
-    // Test for trees
-    
-    // Meant tree : (lambda (x y) x y)
-    using t = tree_node<std::string>;
-
-    t tree{};
-    tree.push_back(t{"lambda"});
-
-    t sub{};
-    sub.push_back(t{"x"});
-    sub.push_back(t{"y"});
-
-    tree.push_back(sub);
-
-    tree.push_back(t{"x"});
-    tree.push_back(t{"y"});
-    
-    std::cout << tree;
-    */
     return 0;
 }
